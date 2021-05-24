bd283738ddef72d58414746a3734039bd010dc75
Cortni <cortni.fernandez@gmail.com>
2021-05-24 13:58:21 -0400

    Fetch working (hard-coded)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      json: {}
+    };
+  }
+
+  getHoroscope() {
+    const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today';
+    fetch(URL, {
+        method: 'POST'
+    }).then(response => response.json())
+    .then(
+      (json) => { 
+        this.setState({
+          isLoaded: true,
+          json
+        }); 
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+}
+
+render() {
+    return (
+      <div>
+      Current Date: {this.state.json.current_date} <br />
+      Compatibility: {this.state.json.compatibility} <br />
+      Lucky Number: {this.state.json.lucky_number} <br />
+      Lucky Time: {this.state.json.lucky_time} <br />
+      Color: {this.state.json.color} <br />
+      Date Range: {this.state.json.date_range} <br />
+      Mood: {this.state.json.mood} <br />
+      Description: {this.state.json.description} <br />
     </div>
-  );
+    );
+}
 }
 
 export default App;
